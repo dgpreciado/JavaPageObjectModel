7b6bc93d790dc0f2e0b6ae1b3957f4c8442eeeff
dgpreciado <37858944+dgpreciado@users.noreply.github.com>
2019-09-07 20:06:24 -0600

    POM ContactPageTest_inProg > got the xpath for the checkbox working @1hr 00min

--- src/test/java/com/crm/qa/testcases/ContactsPageTest.java
+++ src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -5,8 +5,11 @@ import com.crm.qa.pages.ContactsPage;
 import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
 import com.crm.qa.util.TestUtil;
+import org.openqa.selenium.support.CacheLookup;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class ContactsPageTest extends TestBase {
     LoginPage loginPage;
@@ -24,6 +27,16 @@ public class ContactsPageTest extends TestBase {
         loginPage = new LoginPage();
         contactsPage = new ContactsPage();/*might not be needed*/
         homePage = loginPage.login(prop.getProperty("userEmail") , prop.getProperty("password"));
+        homePage.clickOnContactsLink();
+    }
+    @Test
+    public void find_a_contact(){
+        Assert.assertTrue(contactsPage.findContactByName("fname"));
+    }
+    @Test
+    public void select_a_contact() throws InterruptedException {
+        contactsPage.selectContactsByName("fname");
+        Thread.sleep(5000);
     }
     @AfterMethod
     public void tearDown(){
